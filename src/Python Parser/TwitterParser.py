2237300dabd6ef886ddbac7151061d10bd9bdf00
chernandez7 <chernandez7@luc.edu>
2016-01-29 16:43:02 -0600

    Data coming in from twython streaming

--- src/Python Parser/TwitterParser.py
+++ src/Python Parser/TwitterParser.py
@@ -1,29 +1,41 @@
 from twython import Twython
+from twython import TwythonStreamer
 import time
 
+#Initialization of variables
+url = "https://twitter.com/BarackObama"
 APP_KEY = '8tLZ3KImFbikHPHYohJIDez1C'
 APP_SECRET = '1cYBFd7pFPV161hDPLDS7xXCnFmCpPmlPXcxovMKFTly9YWieI'
-
+OAUTH_TOKEN = '4861151506-7ZtKF6s8Ve2mO9bswUIGxNh5OHL4LqvTEkQ3USw'
+OAUTH_TOKEN_SECRET = '4EbmpgVJ2C3tgB99wkkVMAL7eMwF9IfKtP2TWNorOeecy'
 twitter = Twython(APP_KEY, APP_SECRET)
-
-#Initialization of variables
 start = time.time()
-item_count = 0
-url = "https://twitter.com/BarackObama"
+
+class MyStreamer(TwythonStreamer):
+    def on_success(self, data):
+        if 'text' in data:
+            print (data['text'].encode('utf-8'))
+
+    def on_error(self, status_code, data):
+        print (status_code)
+
+        # Want to stop trying to get data because of the error?
+        # Uncomment the next line!
+        #self.disconnect()
+        
 
 #Creating and opening file
 file = open("tweets.txt", "w+")
 
+stream = MyStreamer(APP_KEY, APP_SECRET,
+                    OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+stream.statuses.filter(track='obama')
+
 #URL Requests
 print("Parsing '{}'...\n".format(url))
-response = requests.get(url)
-
-soup = BeautifulSoup(response.content, "html.parser")
-print(soup.prettify()) #test
-#Populating arrays of a single page
 
         
 
 file.close()
 end = time.time()
-print ("Finished processing {} items in {} seconds".format(str(item_count), str(end - start)))
+print ("Finished processing in {} seconds".format(str(end - start)))
