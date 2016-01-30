68e42877427278a8c5b386518571effadf5df361
chernandez7 <chernandez7@luc.edu>
2016-01-29 18:08:23 -0600

    Changed input to find a specific user instead of a stream of tweets

--- src/Python Parser/TwitterParser.py
+++ src/Python Parser/TwitterParser.py
@@ -1,38 +1,45 @@
 from twython import Twython
 from twython import TwythonStreamer
 import time
+import json
 
 #Initialization of variables
+start = time.time()
 url = "https://twitter.com/BarackObama"
 APP_KEY = '8tLZ3KImFbikHPHYohJIDez1C'
 APP_SECRET = '1cYBFd7pFPV161hDPLDS7xXCnFmCpPmlPXcxovMKFTly9YWieI'
 OAUTH_TOKEN = '4861151506-7ZtKF6s8Ve2mO9bswUIGxNh5OHL4LqvTEkQ3USw'
 OAUTH_TOKEN_SECRET = '4EbmpgVJ2C3tgB99wkkVMAL7eMwF9IfKtP2TWNorOeecy'
 twitter = Twython(APP_KEY, APP_SECRET)
-start = time.time()
+file = open("tweets.txt", "w+")
 
+'''
 class MyStreamer(TwythonStreamer):
     def on_success(self, data):
         if 'text' in data:
             print (data['text'].encode('utf-8'))
+            for things in data['text'].encode('utf-8'):
+                file.write(str(things))
 
     def on_error(self, status_code, data):
         print (status_code)
-
-        # Want to stop trying to get data because of the error?
-        # Uncomment the next line!
-        #self.disconnect()
-        
+        self.disconnect()
+        '''
 
 #Creating and opening file
-file = open("tweets.txt", "w+")
-
+'''
 stream = MyStreamer(APP_KEY, APP_SECRET,
                     OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
 stream.statuses.filter(track='obama')
-
+'''
 #URL Requests
 print("Parsing '{}'...\n".format(url))
+user_tweets = twitter.get_user_timeline(screen_name='BarackObama',
+                                        include_rts=False)
+for tweet in user_tweets:
+    tweet['text'] = Twython.html_for_tweet(tweet)
+    #print (tweet['text'].encode('utf-8'))
+    file.write(json.dumps(tweet, indent = 4))
 
         
 
